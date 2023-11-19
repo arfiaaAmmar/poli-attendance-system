ffee3a53adc2d98ce86edfce797572b7064134df
Ammar Hazim <ammarhazim.ayob@gmail.com>
2023-11-19 18:42:41 +0800

    fix netlify build issue

--- shared-library/src/declarations/constants.ts
+++ shared-library/src/declarations/constants.ts
@@ -9,8 +9,8 @@ import {
   User,
 } from "./types";
 
-// export const API_BASE_URL = "https://poli-attendance-system.onrender.com" // For deploy
-export const API_BASE_URL = "http://localhost:8888"; // For dev
+export const API_BASE_URL = "https://poli-attendance-system.onrender.com" // For deploy
+// export const API_BASE_URL = "http://localhost:8888"; // For dev
 export const HEADER_TYPE = { "Content-Type": "application/json" };
 
 export const FORM_TYPE = {
